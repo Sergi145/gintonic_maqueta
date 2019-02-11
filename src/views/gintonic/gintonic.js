2979bdfcd0b10db1d9654f46d385fdb7f4074460
sergi145 <sergipicazo14@gmail.com>
2019-02-11 22:59:56 +0100

    ADD component item-list and img for example

--- src/views/gintonic/gintonic.js
+++ src/views/gintonic/gintonic.js
@@ -1,25 +1,2 @@
 import './gintonic.scss';
 
-//menu
-$(document).ready(main);
-var cont=1;
-var nav=$("nav");
-
-function main () {
-    $('.navbar-toggler').click(function(){
-        
-        if(cont == 1){
-            nav.animate({
-                left:'0'
-                
-            });
-            cont=0;
-        }
-        else {
-            cont=1;
-            nav.animate({
-                left:'-100%'
-            });
-        }
-    })
-};
